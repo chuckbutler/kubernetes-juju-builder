cafb532af9dc42d32542baa84b724e72297ba4b0
Charles Butler <chuck@dasroot.net>
2015-05-28 16:38:03 -0400

    Adds cleanup and ENV VAR usage in the container

--- kubes-ci-run.sh
+++ kubes-ci-run.sh
@@ -9,6 +9,9 @@ export KUBERNETES_PROVIDER=juju
 git clone https://github.com/GoogleCloudPlatform/kubernetes.git $HOME/kubernetes
 sudo chown -R ubuntu:ubuntu $HOME/kubernetes
 sudo chown -R ubuntu:ubuntu $HOME/.juju
+
+juju switch $JUJU_CI_ENV
+
 cd $HOME/kubernetes
 
 /bin/bash -c 'ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa'
@@ -17,5 +20,10 @@ gvm use  go1.4 && \
 make all WHAT=cmd/kubectl && \
 cluster/kube-up.sh \
 
+
+juju destroy-environment $JUJU_CI_ENV -y --force
+
+# Our credentials are ephemeral, so remove them
+rm -rf ~/.juju
 
 
