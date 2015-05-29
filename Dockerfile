4abeecf0047fecd782743a9a088b923dfb768263
Charles Butler <chuck@dasroot.net>
2015-05-29 00:16:55 -0400

    Distill away complexity, and try again

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,15 @@ FROM johnsca/charmbox
 
 MAINTAINER Charles Butler <charles.butler@canonical.com>
 
+ENV JUJU_CI_ENV=aws-kubes-ci
+
 RUN apt-get update
 RUN apt-get install -y curl git mercurial make binutils bison gcc build-essential
 RUN apt-get upgrade -y
 ADD install-gvm.sh /tmp/install-gvm.sh
 RUN /tmp/install-gvm.sh
-run sudo -u ubuntu /bin/bash -c 'ssh-keygen -t rsa -N "" -f /home/ubuntu/.ssh/id_rsa'
 
-ENV JUJU_CI_ENV=aws-kubes-ci
 ADD kubes-ci-run.sh /kubes-ci-run.sh
 
-
 ENTRYPOINT
-
-RUN chown -R ubuntu:ubuntu /home/ubuntu/.juju
 CMD sudo -u ubuntu /bin/bash -c "/kubes-ci-run.sh"
--- kubes-ci-run.sh
+++ kubes-ci-run.sh
@@ -8,41 +8,12 @@ export KUBERNETES_PROVIDER=juju
 
 git clone https://github.com/GoogleCloudPlatform/kubernetes.git $HOME/kubernetes
 sudo chown -R ubuntu:ubuntu $HOME/kubernetes
+sudo chown -R ubuntu:ubuntu $HOME/.juju
 
-if [ -f '/tmp/juju-credentials.tar.gz' ]; then
-    sudo cp /tmp/juju-credentials.tar.gz .
-    sudo chown ubuntu:ubuntu juju-credentials.tar.gz
-    mkdir -p .juju
-    tar xvfz juju-credentials.tar --strip-components=1 -C .juju
-
-    # Randomly, it seems, the .juju directory loses permissions
-    sudo chown -R ubuntu:ubuntu $HOME/.juju
-fi
-
-
-if [ ! -f '/home/ubuntu/juju/environments.yaml' ]; then
-    echo "No environment configuration found, exiting"
-    exit 1
-fi
-
-juju switch $JUJU_CI_ENV
-
-# Forcibly destroy the environment, because things happen and stuff
-# gets inexplicably stuck when jobs go awry
-juju destroy-environment $JUJU_CI_ENV --force || true
 
 cd $HOME/kubernetes
-
-# Allow failure so we always reach cleanup
-set +e
 
 gvm use  go1.4 && \
 make all WHAT=cmd/kubectl && \
 cluster/kube-up.sh \
-
-juju destroy-environment $JUJU_CI_ENV -y --force || true
-
-# Our credentials are ephemeral, so remove them
-rm -rf ~/.juju
-
 
